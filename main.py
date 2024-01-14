2e6c6bcdc0c06152bd83df9ce79b2b4adebd8b4b
techsimplenl <techsimplenl@gmail.com>
2024-01-14 19:42:34 +0100

    3de commit

--- main.py
+++ main.py
@@ -1,63 +1,14 @@
 """RUN THIS FILE BY python main.py"""
-import matplotlib
-from Entity.Weather import WeatherEntity
-from private.request import GetApiFromWeather
-from operations.operations import Operations
-from operations.save_data_to_jsonfile import resenting_data_with_matplot
-
 import os
+import matplotlib
+from operations.operations import Operations
 from dotenv import load_dotenv
-
-
 matplotlib.use('TkAgg')  # Use the TkAgg backend
-
 load_dotenv()
-
-def reutrn_data_to_save(weather:WeatherEntity):
-    """data to save""" 
-    return weather.__dict__
-# set the entity values
-def retrieve_important_forecast(data_: any,weather: any):
-    """Class representing Weather"""
-    # Iterating through the outer dictionary
-    for item, details in data_.items():
-        if 'location' == item:
-            weather.country = {
-                'name':details['country'],
-                'city':details['name'],
-                'region':details['region'],
-            }
-            weather.time = details['localtime']   
-        if 'current' == item:
-            weather.current = {
-                'current_temp_c': details['temp_c'],
-                'current_temp_f': details['temp_f'],
-                'last_updated': details['last_updated'],
-                'wind_dir': details['wind_dir'],
-                'pressure_mb': details['pressure_mb'],
-                'pressure_in': details['pressure_in'],
-                'cloud': details['cloud'],
-                'gust_mph': details['gust_mph'],
-                'gust_kph': details['gust_kph'],
-                }
-            weather.humidity = details['humidity']
-            # weather.city = details['name']
-        if 'forecast' == item:
-            weather.forecast = details['forecastday']
-def py():
-    """All variables and functions here"""
-    base_url = 'https://api.weatherapi.com/v1'
-    url = base_url +"/forecast.json"
-    api_key = os.environ.get('API-KEY') #here
-    params = {"key": api_key,"q":'38.5934,0.6725','days':14}
-    result = GetApiFromWeather(url,params).get_api_data()
-    if result:
-        # here we have to save this data in files
-        return result
-    return None
-# Check if this script is being run as the main program
 if __name__ == "__main__":
-    data = py() # run main function
-    Operations.retrieve_important_forecast(data_=data,weather=WeatherEntity)
-    # save_data_into_json_file()
-    resenting_data_with_matplot()
+    # data = py() # run main function
+    URI = "https://api.weatherapi.com/v1/forecast.json"
+    api_key = os.environ.get('API-KEY') #here
+    params = {"key": api_key,"q":'38.5934,0.6725','days':5}
+    args={'url':URI,'params':params}
+    Operations(args).presenting_data_with_matplot()
--- operations/operations.py
+++ operations/operations.py
@@ -1,14 +1,71 @@
 """Operation program"""
+from private.request import GetApiFromWeather
+from Entity.Weather import WeatherEntity
+import matplotlib.pyplot as plt
 class Operations:
     """All operations"""
-    @staticmethod
-    def retrieve_important_forecast(data_: dict,weather:any):
+    def __init__(self, args):
+        self.__url = args['url']
+        self.__params = args['params']
+        self.__weather = WeatherEntity
+    
+    def get_data_from_api(self):
+        """Retrieve Data from API."""
+        result = GetApiFromWeather(self.__url,self.__params).get_api_data()
+        if result:
+        # here we have to save this data in files
+            return result
+        return None
+
+    def retrieve_important_forecast(self):
         """Retrieve important forecast information from the given data."""
+        data_ = self.get_data_from_api()
         for item, details in data_.items():
             if 'location' == item:
-                weather.assign_location(weather,details)
+                self.__weather.assign_location(self.__weather,details)
             if 'current' == item:
-                weather.assign_current(weather,details)
+                self.__weather.assign_current(self.__weather,details)
             if 'forecast' == item:
-                weather.assign_forecast(weather,details)
-    
+                self.__weather.assign_forecast(self.__weather,details)
+
+    def design_plot_with_matlib(self,**kwargs):
+        """Plot for represententing the temperature trends per day in your region"""
+        # Convert dates to numerical values for plotting
+        print(kwargs)
+        numeric_dates = range(len(kwargs['list_dates']))
+        #Plotting the data
+        plt.plot(numeric_dates, kwargs['list_max_temp_c'], label='Max Temperature', marker='o')
+        plt.plot(numeric_dates, kwargs['list_min_temp'], label='Min Temperature', marker='o')
+        plt.plot(numeric_dates, kwargs['list_average_temp'], label='Average Temperature', marker='o')
+        #Adding labels and tittle
+        plt.xlabel("Dates")
+        plt.ylabel("Temperature (°C)")
+        plt.title(f"Temperature trends from : {kwargs['city']} - {kwargs['name']}")
+        plt.xticks(numeric_dates,kwargs['list_dates']) # Use actual dates as x-axis labels
+        plt.legend() # Display legend
+        # plt.savefig('my_plot.png')
+        # Display the plot
+        plt.show()
+
+    def  presenting_data_with_matplot(self):
+        """RESETING THE LIST OF WEATHER ENTITIES AND CALL"""
+        self.retrieve_important_forecast()
+        forecast = self.__weather.forecast
+        list_dates = []
+        list_average_temp = []
+        list_min_temp = []
+        list_max_temp_c = []
+        for index,item in enumerate(forecast):
+            list_dates.append(item['date'])
+            list_max_temp_c.append(item['day']['maxtemp_c'])
+            list_average_temp.append(item['day']['avgtemp_c'])
+            list_min_temp.append(item['day']['mintemp_c'])
+        kwargs = {
+            'list_dates':list_dates,
+            'list_average_temp':list_average_temp,
+            'list_min_temp':list_min_temp,
+            'list_max_temp_c':list_max_temp_c,
+            'city': self.__weather.country['city'],
+            'name': self.__weather.country['name'] 
+        }
+        self.design_plot_with_matlib(**kwargs)
